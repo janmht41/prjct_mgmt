19e138edcc6b006337e86437f1e33694a375d6da
Mohit Jain <janmht41@gmail.com>
2022-08-29 23:49:12 +0530

    added client side stuff

---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,18 @@
+import { FaExclamationTriangle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <div
+      className="display-flex flex-column 
+    justify-content-center align-items-center mt-5"
+    >
+      <FaExclamationTriangle className="text-danger" size={"5cm"} />
+      <h1>404</h1>
+      <p className="lead">Sorry this page does not exist</p>
+      <Link to="/" className="btn btn-primary">
+        Back to Home
+      </Link>
+    </div>
+  );
+}
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const colors = require("colors");
+const cors = require("cors");
 const connectDB = require("./config/db");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
+app.use(cors());
 
 app.use(
   "/graphql",
